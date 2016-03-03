e42ed3a9ba0c7de09bea8eecf5a69ffbced28a39
d-fournier <donof43@gmail.com>
2016-03-03 14:23:27 +0100

    Add ViewPager

--- app/src/main/java/fr/sims/coachingproject/ui/activity/FirstLaunchActivity.java
+++ app/src/main/java/fr/sims/coachingproject/ui/activity/FirstLaunchActivity.java
@@ -20,23 +20,30 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
+import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.viewpagerindicator.CirclePageIndicator;
+import com.viewpagerindicator.TitlePageIndicator;
+
 import fr.sims.coachingproject.R;
 import fr.sims.coachingproject.ui.adapter.FirstLaunchPagerAdapter;
+import fr.sims.coachingproject.util.SharedPrefUtil;
 
 public class FirstLaunchActivity extends AppCompatActivity implements ViewPager.OnPageChangeListener, View.OnClickListener {
 
     private FirstLaunchPagerAdapter mSectionsPagerAdapter;
 
     private ViewPager mViewPager;
-    private ImageButton mNextBtn;
-    private ImageButton mPreviousBtn;
+
     private ImageView mBkg1IV;
     private ImageView mBkg2IV;
-    TypedArray mBkgIds;
+    private TypedArray mBkgIds;
+    private Button mContinueBtn;
+
+    private int mPreviousPosition = -1;
 
 
     public static void startActivity(Context ctx){
@@ -55,20 +62,22 @@ public class FirstLaunchActivity extends AppCompatActivity implements ViewPager.
         mViewPager.setAdapter(mSectionsPagerAdapter);
         mViewPager.addOnPageChangeListener(this);
 
-        mNextBtn = (ImageButton) findViewById(R.id.first_launch_next);
-        mPreviousBtn = (ImageButton) findViewById(R.id.first_launch_previous);
         mBkg1IV = ((ImageView) findViewById(R.id.first_launch_bkg1));
         mBkg2IV = ((ImageView) findViewById(R.id.first_launch_bkg2));
 
-        updateBtn(0);
+        mContinueBtn = (Button) findViewById(R.id.first_launch_continue);
+        mContinueBtn.setOnClickListener(this);
 
+        CirclePageIndicator indicator = (CirclePageIndicator) findViewById(R.id.first_launch_indicator);
+        indicator.setViewPager(mViewPager);
+
+        setupBtn(0);
     }
 
     @Override
     protected void onStart() {
         super.onStart();
         mBkgIds = getResources().obtainTypedArray(R.array.first_launch_image);
-
     }
 
     @Override
@@ -79,10 +88,9 @@ public class FirstLaunchActivity extends AppCompatActivity implements ViewPager.
 
     @Override
     public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
-        int id1 = mBkgIds.getResourceId(position, -1);
-        int id2 = mBkgIds.getResourceId(position+1,-1);
         ImageView img1;
         ImageView img2;
+
         if(position % 2 == 0) {
             img1 = mBkg1IV;
             img2 = mBkg2IV;
@@ -91,54 +99,43 @@ public class FirstLaunchActivity extends AppCompatActivity implements ViewPager.
             img2 = mBkg1IV;
         }
 
-        if(id1 != -1)
-            img1.setImageResource(id1);
-        if(id2 != -1)
-            img2.setImageResource(id2);
+        if(mPreviousPosition != position) {
+            int id1 = mBkgIds.getResourceId(position, -1);
+            int id2 = mBkgIds.getResourceId(position+1,-1);
+
+            if(id1 != -1)
+                img1.setImageResource(id1);
+            if(id2 != -1)
+                img2.setImageResource(id2);
+            mPreviousPosition = position;
+        }
 
 
-        img1.setAlpha(1-positionOffset);
+        img1.setAlpha(1 - positionOffset);
         img2.setAlpha(positionOffset);
     }
 
     @Override
     public void onPageSelected(int position) {
-        updateBtn(position);
+        setupBtn(position);
+    }
+
+    private void setupBtn(int position){
+        if(position == mSectionsPagerAdapter.getCount()-1) {
+            mContinueBtn.setVisibility(View.VISIBLE);
+        } else {
+            mContinueBtn.setVisibility(View.GONE);
+        }
+
     }
 
     @Override
-    public void onPageScrollStateChanged(int state) {  }
-
-    private void updateBtn(int position){
-        int itemNumber = mSectionsPagerAdapter.getCount();
-
-        if(position > 0) {
-            mPreviousBtn.setVisibility(View.VISIBLE);
-            mPreviousBtn.setOnClickListener(this);
-        } else {
-            mPreviousBtn.setVisibility(View.GONE);
-            mPreviousBtn.setOnClickListener(null);
-        }
-        if(position < itemNumber-1) {
-            mNextBtn.setVisibility(View.VISIBLE);
-            mNextBtn.setOnClickListener(this);
-        } else {
-            mNextBtn.setVisibility(View.GONE);
-            mNextBtn.setOnClickListener(null);
-        }
-
-    }
+    public void onPageScrollStateChanged(int state) { }
 
     @Override
     public void onClick(View v) {
-        int viewId = v.getId();
-        switch (viewId){
-            case R.id.first_launch_next:
-                mViewPager.setCurrentItem(mViewPager.getCurrentItem()+1, true);
-                break;
-            case R.id.first_launch_previous:
-                mViewPager.setCurrentItem(mViewPager.getCurrentItem()-1, true);
-                break;
-        }
+        SharedPrefUtil.putIsFirstLaunch(this, false);
+        MainActivity.startActivity(this);
+        finish();
     }
 }
