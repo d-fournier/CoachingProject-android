8848ebb383075ef3b96b1eb94b4aaf71f3eb067d
Benjamin Legrand <benjamin.legrand93@gmail.com>
2016-03-17 01:03:58 +0100

    Corrected little problem on search button

--- app/src/main/java/fr/sims/coachingproject/ui/activity/MainActivity.java
+++ app/src/main/java/fr/sims/coachingproject/ui/activity/MainActivity.java
@@ -103,8 +103,6 @@ public class MainActivity extends AppCompatActivity
                         SearchActivity.startActivity(getApplicationContext(), false, -1, true);
                         break;
                     case 1:
-                        break;
-                    case 2:
                         SearchGroupActivity.startActivity(getApplicationContext());
                         break;
                 }
